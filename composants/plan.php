f256c85dba2d60843000d85b7128e39ad3cfa062
equipevisiosarl <equipevisio@gmail.com>
2024-03-04 00:18:52 +0000

    pricing

--- composants/plan.php
+++ composants/plan.php
@@ -1,103 +1,117 @@
-<section class="section category" aria-label="category" id="plan">
-        <div class="container">
+<section class="section category" aria-label="category" id="plan" style="margin-top: -130px;">
+    <div class="container">
 
-          <p class="section-subtitle">Nos Offres & Plans d'accompagnements</p>
+        <p class="section-subtitle">Nos Offres & Plans d'accompagnements</p>
 
-          <h2 class="h2 section-title">
-          Découvrez nos offres exclusives<span class="span"> pour réussir vos études aux États-Unis</span> 
-          </h2>
+        <h2 class="h2 section-title">
+            Découvrez nos offres exclusives<span class="span"> pour réussir vos études aux États-Unis</span>
+        </h2>
 
-          <p class="section-text">
-          Accompagnement sur mesure à partir de X$, ne laissez pas passer cette opportunité unique pour atteindre vos objectifs éducatifs !
-          </p>
+        <p class="section-text">
+            Accompagnement sur mesure à partir de X$, ne laissez pas passer cette opportunité unique pour atteindre vos objectifs éducatifs !
+        </p>
 
-          <ul class="grid-list">
+        <ul class="grid-list">
 
             <li>
-              <div class="category-card" style="--color: 170, 75%, 41%">
+                <div class="category-card" style="--color: 170, 75%, 41%">
 
-                <div class="card-icon">
-                  <img src="./assets/images/category-1.svg" width="40" height="40" loading="lazy"
-                    alt="Online Degree Programs" class="img">
+                    <div class="card-icon">
+                        <img src="./assets/images/category-1.svg" width="40" height="40" loading="lazy" alt="Online Degree Programs" class="img">
+                    </div>
+
+                    <h3 class="h3">
+                        <a href="#" class="card-title">Consultation</a>
+                    </h3>
+
+                    <p class="card-text">
+                        Consultation avec les parents, l'enfant et le financeur des études. Je me réserve le droit de choisir les familles avec lesquelles je collaborerai.
+                    </p>
+
+                    <span class="card-badge">100 000Fcfa</span>
+
                 </div>
-
-                <h3 class="h3">
-                  <a href="#" class="card-title">Online Degree Programs</a>
-                </h3>
-
-                <p class="card-text">
-                  Lorem ipsum dolor consec tur elit adicing sed umod tempor.
-                </p>
-
-                <span class="card-badge">7 Courses</span>
-
-              </div>
             </li>
 
             <li>
-              <div class="category-card" style="--color: 351, 83%, 61%">
+                <div class="category-card" style="--color: 351, 83%, 61%">
 
-                <div class="card-icon">
-                  <img src="./assets/images/category-2.svg" width="40" height="40" loading="lazy"
-                    alt="Non-Degree Programs" class="img">
+                    <div class="card-icon">
+                        <img src="./assets/images/category-2.svg" width="40" height="40" loading="lazy" alt="Non-Degree Programs" class="img">
+                    </div>
+
+                    <h3 class="h3">
+                        <a href="#" class="card-title">Bronze</a>
+                    </h3>
+
+                    <p class="card-text">
+                        L'étudiant, sa famille et moi-même postulons ensemble à deux universités américaines de notre choix. Voici les points clés de notre processus :
+
+                    <ul>
+                        <li><b>*</b> Collaboration dans le choix des universités. <br></li>
+                        <li><b>*</b> Gestion complète de l'application à partir des documents de la famille. <br></li>
+                        <li><b>*</b> Possibilité de remboursement de 75% en cas de problème d'admission malgré la soumission de tous les documents à temps.</li>
+                    </ul>
+
+                    </p>
+
+                    <span class="card-badge">600 000Fcfa</span>
+
                 </div>
-
-                <h3 class="h3">
-                  <a href="#" class="card-title">Non-Degree Programs</a>
-                </h3>
-
-                <p class="card-text">
-                  Lorem ipsum dolor consec tur elit adicing sed umod tempor.
-                </p>
-
-                <span class="card-badge">4 Courses</span>
-
-              </div>
             </li>
 
             <li>
-              <div class="category-card" style="--color: 229, 75%, 58%">
+                <div class="category-card" style="--color: 229, 75%, 58%">
 
-                <div class="card-icon">
-                  <img src="./assets/images/category-3.svg" width="40" height="40" loading="lazy"
-                    alt="Off-Campus Programs" class="img">
+                    <div class="card-icon">
+                        <img src="./assets/images/category-3.svg" width="40" height="40" loading="lazy" alt="Off-Campus Programs" class="img">
+                    </div>
+
+                    <h3 class="h3">
+                        <a href="#" class="card-title">Silver</a>
+                    </h3>
+
+                    <p class="card-text">
+                    Le package Silver comprend tous les avantages du package Bronze ainsi que :
+                    <ul>
+                        <li>* Assistance à la prise de rendez-vous pour l'entretien de visa étudiant international.</li>
+                        <li>* Coaching de l'étudiant pour la préparation de l'entrevue.</li>
+                        <li>* Remboursement de 25% en cas de refus de visa.</li>
+                    </ul>
+                    </p>
+                    <br><br><br>
+
+                    <span class="card-badge">1 millions</span>
+
                 </div>
-
-                <h3 class="h3">
-                  <a href="#" class="card-title">Off-Campus Programs</a>
-                </h3>
-
-                <p class="card-text">
-                  Lorem ipsum dolor consec tur elit adicing sed umod tempor.
-                </p>
-
-                <span class="card-badge">8 Courses</span>
-
-              </div>
             </li>
 
             <li>
-              <div class="category-card" style="--color: 42, 94%, 55%">
+                <div class="category-card" style="--color: 42, 94%, 55%">
 
-                <div class="card-icon">
-                  <img src="./assets/images/category-4.svg" width="40" height="40" loading="lazy"
-                    alt="Hybrid Distance Programs" class="img">
+                    <div class="card-icon">
+                        <img src="./assets/images/category-4.svg" width="40" height="40" loading="lazy" alt="Hybrid Distance Programs" class="img">
+                    </div>
+
+                    <h3 class="h3">
+                        <a href="#" class="card-title">Gold</a>
+                    </h3>
+
+                    <p class="card-text">
+                    Le package Golde comprend tous les avantages des packages Silver ainsi que:
+                    <ul>
+                        <li>* Assistance à l'intégration de l'étudiant dans sa nouvelle université.</li>
+                        <li>* Aide à la réservation du billet d'avion et à la logistique des premiers 7 jours.</li>
+                        <li>* Accompagnement dans la prise des cours de l'étudiant et recherche d'un appartement pour lui/elle.</li>
+                    </ul>
+                    </p>
+
+                    <span class="card-badge">1 600 000Fcfa</span>
+
                 </div>
-
-                <h3 class="h3">
-                  <a href="#" class="card-title">Hybrid Distance Programs</a>
-                </h3>
-
-                <p class="card-text">
-                  Lorem ipsum dolor consec tur elit adicing sed umod tempor.
-                </p>
-
-                <span class="card-badge">8 Courses</span>
-
-              </div>
             </li>
 
-          </ul>
+        </ul>
 
-        </div>
-      </section>
+    </div>
+</section>
